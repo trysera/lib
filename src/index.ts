662dbb81b5a17d49f1a7999548001c7d634f4708
John Letey <johnletey@gmail.com>
2020-12-08 19:55:08 +0000

    feat: even more functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import Arweave from "arweave";
 import { all, run } from "ar-gql";
 import createQuery from "./queries/create.gql";
 import shareQuery from "./queries/share.gql";
@@ -5,6 +6,22 @@ import removeQuery from "./queries/remove.gql";
 import editQuery from "./queries/edit.gql";
 import createWithIDQuery from "./queries/createWithID.gql";
 import shareWithIDQuery from "./queries/shareWithID.gql";
+import { JWKInterface } from "arweave/node/lib/wallet";
+import {
+  getDecryptionKey,
+  decryptData,
+  encryptData,
+  getEncryptionKey,
+} from "./encryption";
+// @ts-ignore
+import limestone from "@limestonefi/api";
+import { nanoid } from "nanoid";
+
+const client = new Arweave({
+  host: "arweave.net",
+  port: 443,
+  protocol: "https",
+});
 
 const getIDs = async (
   addr: string
@@ -64,4 +81,145 @@ const getLatest = async (
   } else {
     return { tx: edits[0].node.id, mined: edits[0].node.block ? true : false };
   }
+};
+
+export const getTx = async (tx: string, jwk: JWKInterface) => {
+  const key = await getDecryptionKey(jwk);
+  const data = await client.transactions.getData(tx, { decode: true });
+
+  // @ts-ignore
+  return await decryptData(data, key);
+};
+
+export const getPasswords = async (jwk: JWKInterface) => {
+  const addr = await client.wallets.jwkToAddress(jwk);
+
+  const res: {
+    id: string;
+    shared: boolean;
+    tx: string;
+    mined: boolean;
+    site: string;
+    username: string;
+    password: string;
+  }[] = [];
+
+  for (const elem of await getIDs(addr)) {
+    const { tx, mined } = await getLatest(elem.id, addr);
+    const data = await getTx(tx, jwk);
+
+    res.push({
+      id: elem.id,
+      shared: elem.shared,
+      tx,
+      mined,
+      ...data,
+    });
+  }
+
+  return res;
+};
+
+export const getFee = async (
+  method: "create" | "share",
+  addr?: string
+): Promise<number> => {
+  let fee = 0;
+  if (method === "create" && addr) {
+    const createTxs = await all(createQuery, { addr });
+    if (createTxs.length >= 2) fee = 0.5;
+  }
+  if (method === "share") fee = 0.25;
+
+  if (fee === 0) return 0;
+
+  const price = 1 / (await limestone.getPrice("AR")).price;
+  return price * fee;
+};
+
+export const create = async (
+  data: {
+    site: string;
+    username: string;
+    password: string;
+  },
+  jwk: JWKInterface
+): Promise<string> => {
+  const addr = await client.wallets.jwkToAddress(jwk);
+  const fee = await getFee("create", addr);
+
+  const res = await encryptData(data, (await getEncryptionKey(addr))!);
+
+  const tx = await client.createTransaction(
+    fee > 0
+      ? {
+          target: "68JAKmnxrHOiNSXGzBk3O8Z7Mxdy3l4wUdJpnPlsNLw",
+          quantity: client.ar.arToWinston(fee.toString()),
+          data: res,
+        }
+      : {
+          data: res,
+        },
+    jwk
+  );
+
+  tx.addTag("App-Name", "Sera");
+  tx.addTag("ID", nanoid());
+  tx.addTag("Action", "Create");
+
+  await client.transactions.sign(tx, jwk);
+  await client.transactions.post(tx);
+
+  return tx.id;
+};
+
+export const share = async (
+  id: string,
+  target: string,
+  jwk: JWKInterface
+): Promise<string | undefined> => {
+  const addr = await client.wallets.jwkToAddress(jwk);
+  const fee = await getFee("share");
+  if (
+    fee >
+    parseFloat(client.ar.winstonToAr(await client.wallets.getBalance(addr)))
+  )
+    return;
+
+  const feeTx = await client.createTransaction(
+    {
+      target: "68JAKmnxrHOiNSXGzBk3O8Z7Mxdy3l4wUdJpnPlsNLw",
+      quantity: client.ar.arToWinston(fee.toString()),
+    },
+    jwk
+  );
+
+  feeTx.addTag("App-Name", "Sera");
+  feeTx.addTag("ID", id);
+  feeTx.addTag("Action", "Share-Fee");
+
+  await client.transactions.sign(feeTx, jwk);
+  await client.transactions.post(feeTx);
+
+  const res = await encryptData(
+    await getTx((await getLatest(id, addr)).tx, jwk),
+    (await getEncryptionKey(target))!
+  );
+
+  const tx = await client.createTransaction(
+    {
+      target,
+      data: res,
+    },
+    jwk
+  );
+
+  tx.addTag("App-Name", "Sera");
+  tx.addTag("ID", id);
+  tx.addTag("Action", "Share");
+
+  await client.transactions.sign(tx, jwk);
+  await client.transactions.post(tx);
+
+  return tx.id;
 };
